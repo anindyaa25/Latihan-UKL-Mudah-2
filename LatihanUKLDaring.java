c0d47ef037d4dee7b5e67e261c24a7875b7fb8f8
anindyaa25 <nabilaanindya579@gmail.com>
2025-11-05 18:21:46 +0700

    Add files via upload

---
+++ LatihanUKLDaring.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanukldaring;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nabila Anindya
+ */
+public class LatihanUKLDaring {
+
+    public static void main(String[] args) {
+        {
+            {
+                
+                
+         // nomer 1 kedua ganjil genap
+         Scanner masukkan = new Scanner(System.in);
+        int bilangan1;
+        System.out.print("Masukkan bilangan : ");
+        bilangan1 = masukkan.nextInt();
+        if (bilangan1 % 2 == 0) {
+            System.out.println("Bilangan genap");
+            System.out.println("Bilangan ini bisa dibagi 2");
+        } else {
+            System.out.println("Bilangan ganjil");
+        }
+       
+    }
+    } 
+    }
+}
+
+    
